4c0111b038ec26381691f1955f4374b40503d30e
Eric Day <Eric.Day@fyidoctors.com>
2019-10-02 12:09:33 -0600

    Episode 30

---
+++ tests/Feature/InvitationsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class InvitationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_project_can_invite_a_user()
+    {
+        // Given a project
+        $project = ProjectFactory::create();
+
+        // owner of project invites a user
+        $project->invite($newUser = factory(User::class)->create());
+
+        //  then, new user will have permission to add tasks
+        $this->signIn($newUser);
+        $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'Foo task']);
+
+        $this->assertDatabaseHas('tasks', $task);
+    }
+}
