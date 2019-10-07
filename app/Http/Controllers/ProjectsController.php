3fde7b04c98a65fdbc7e8f01bc6d6641abd45035
eday <eric.day@fyidoctors.com>
2019-10-07 09:07:26 -0600

    Episode 40

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -15,6 +15,10 @@ class ProjectsController extends Controller
     public function store() {
         $project = auth()->user()->projects()->create($this->validateRequest());
 
+        if (request()->wantsJson()) {
+            return ['message' => $project->path() ];
+        }
+
         return redirect($project->path());
     }
 
